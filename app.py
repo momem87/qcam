20e0d71b4b33bf4c98be36778ab98e1c5ebad43c
momem87 <momem8501@gmail.com>
2026-02-01 05:01:28 +0200

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -9,3 +10,7 @@ def home():
 @app.route("/api/health")
 def health():
     return jsonify(status="ok")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
