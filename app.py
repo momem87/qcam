65318c36aa052c4b74e198fd288802e8328d0838
momem87 <momem8501@gmail.com>
2026-02-01 07:00:05 +0200

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 
 app = Flask(__name__)
 
-RPI_URL = "https://citizenship-determination-invitations-instance.trycloudflare.com"
+RPI_URL = "https://preston-tremendous-dozens-organizer.trycloudflare.com"
 
 @app.route('/')
 def index():
